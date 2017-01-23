14b9f5391b5a6e1410f88d41cc6e9c6da3b11fc6
davydovaolga <olgita.davydova@gmail.com>
2017-01-23 18:53:41 +0300

    Исправила замечания

--- js/stat.js
+++ js/stat.js
@@ -14,40 +14,37 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillText('Ура вы победили!', 120, 40);
     ctx.fillText('Список результатов:', 120, 60);
 
-    var max = 1;
+var max = 1;
 
- for (var i = 0 ; i < times.length; i++ ) {
-   var time = times[i];
-   if (time > max) {
-   max = time;
-   }
- }
+for (var i = 0 ; i < times.length; i++ ) {
+    var time = times[i];
+    if (time > max) {
+    max = time;
+    }
+}
 
-   var histoHeight = 150;
-   var histoX = 140;
-   var step = histoHeight / max;
-   var columnIndent = 90;
+var histogrammaHeight = 150;
+var histogrammaX = 140;
+var step = histogrammaHeight / max;
+var columnIndent = 90;
 
-   for (var i = 0; i < times.length; i++) {
+for (var i = 0; i < times.length; i++) {
      var name = names[i];
      var time = times[i];
+     var height = step * time;
+     ctx.fillStyle = '#000';
+     ctx.fillText(time.toFixed(0), histogrammaX + columnIndent * i, 90 + histogrammaHeight - height );
      if ( name === 'Вы') {
-         var height = step * time;
-         ctx.fillStyle = '#000';
-         ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 90 + histoHeight - height );
-         ctx.fillStyle = '#FF0000';
-         ctx.fillRect(histoX + columnIndent * i, 100 + histoHeight - height, 40, height);
-         ctx.fillStyle = '#000';
-         ctx.fillText(name, histoX + columnIndent * i, 100 + histoHeight + 20);
+        ctx.fillStyle = '#FF0000';
+        ctx.fillRect(histogrammaX + columnIndent * i, 100 + histogrammaHeight - height, 40, height);
+        ctx.fillStyle = '#000';
+        ctx.fillText(name, histogrammaX + columnIndent * i, 100 + histogrammaHeight + 20);
      } else {
-         var height = step * time;
-         ctx.fillStyle = '#000';
-         ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 90 + histoHeight - height);
-         var color = Math.floor(Math.random() * 244) + 2;
-         ctx.fillStyle = 'rgba(0,0,' + color + ', '+ Math.random() +')';
-         ctx.fillRect(histoX + columnIndent * i, 100 + histoHeight - height, 40, height);
-         ctx.fillStyle = '#000';
-         ctx.fillText(name, histoX + columnIndent * i, 100 + histoHeight + 20);
-     }
+        var color = Math.floor(Math.random() * 244) + 2;
+        ctx.fillStyle = 'rgba(0,0,' + color + ', '+ Math.random() +')';
+        ctx.fillRect(histogrammaX + columnIndent * i, 100 + histogrammaHeight - height, 40, height);
+        ctx.fillStyle = '#000';
+        ctx.fillText(name, histogrammaX + columnIndent * i, 100 + histogrammaHeight + 20);
+        }
     }
 };
